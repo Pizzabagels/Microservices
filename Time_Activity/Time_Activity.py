183a17cdc2b75a7c4d6434e1fd38bc6da5c92821
Tommy <taguilu01@gmail.com>
2020-05-04 23:24:17 -0700

    first commit

---
+++ Time_Activity/Time_Activity.py
@@ -0,0 +1,14 @@
+import os
+import time
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/gettime/')
+def epoch_time():
+    return str(time.time().__round__())
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 6738))
+    app.run(host='0.0.0.0', port=port)
